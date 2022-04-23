aadc9b4a9069400db3db1e6d718ec5e8b07a84d0
Ayeni Clement <ayeniayokunle@gmail.com>
2022-04-23 16:28:50 +0100

    i just addedd projet files

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app =express();
+const port=process.env.PORT || 3000
+app.get('/',(req, res)=>{
+    res.send('<h1>Exppress Demo App By Ayeni clement</h1> <h4> Message: Success</h4> <p>version 1.0</p>');
+})
+
+
+app.listen(port, ()=> {
+console.log('Node app app is up and listening to port: 3000');
+}) 
